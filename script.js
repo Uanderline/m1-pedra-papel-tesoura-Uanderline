fe55baa1e0544660f8fbb27f8b29ed87d680680d
Uanderline <45636326+Uanderline@users.noreply.github.com>
2024-07-08 18:23:29 -0300

    Desenvolvimento da function playRockPaperScissor

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 /* Desenvolva seu código abaixo */
 console.log("Primeira entrega!")
+
+function playRockPaperScissor(jogador1, jogador2) {
+    if (jogador1 === jogador2) {
+        return 'Empate!';
+    }
+
+    if (
+        (jogador1 === 'Pedra' && jogador2 === 'Tesoura') || 
+        (jogador1 === 'Papel' && jogador2 === 'Pedra') ||
+        (jogador1 === 'Tesoura' && jogador2 === 'Papel')
+    ) {
+        return 'Jogador 1 venceu!';
+    } else {
+        return 'Jogador 2 venceu!';
+    }
+}
+
